07fe6a2ccecdc3d2fde955f83b27a51966fc418e
Joshua-Dugas <dugas93@gmail.com>
2025-02-01 16:13:13 -0600

    added asteroid and shot objects, collision detection, and finished game loop

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import pygame
-
-from Asteroids.player import Player
-from constants import *
-from pygame.time import Clock
-
-from constants import *
-
-
+import sys
+from Asteroids.asteroidfield import AsteroidField
+from Asteroids.shot import Shot
+from asteroid import Asteroid
 import pygame
 from constants import *
 from player import Player
@@ -16,24 +11,54 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-    updatables = pygame.sprite.Group()
+
+    #Create object groups
+    updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    Player.containers = (updatables, drawable)
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    #setup containers
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+
+    #initialize objects
+    asteroid_field = AsteroidField()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    dt = 0
+
+    #Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        # limit the framerate to 60 FPS
-        dt = clock.tick(60) / 1000
+
+        updatable.update(dt)
+
+        #Game over check
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
-        updatables.update(dt)
+
+        #Checks for asteroid and shot collision
         for obj in drawable:
             obj.draw(screen)
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
