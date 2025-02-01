1294576fa60f1f41ba53dce2f19df7ef206c213c
Joshua-Dugas <dugas93@gmail.com>
2025-02-01 14:27:36 -0600

    finished creation and implementation of the player object/movement

--- main.py
+++ main.py
@@ -16,20 +16,24 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+    updatables = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatables, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
-
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
+
+        screen.fill("black")
+        updatables.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
